17d891e2eefa7c1f45cae8ebfab93f65ebf92f7b
matteosister <matteog@gmail.com>
2012-10-17 10:31:22 +0200

    playground items

---
+++ tests/Walrus/Theme/ThemeTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: matteo
+ * Date: 17/10/12
+ * Time: 9.58
+ *
+ * Just for fun...
+ */
+
+namespace Walrus\Theme;
+
+use Walrus\WalrusTestCase,
+    Walrus\Theme\Theme;
+
+class ThemeTest extends WalrusTestCase
+{
+    public function testConstructor()
+    {
+        $t = new Theme($this->fixturesDir.'/theme', $this->getMockAssetCollection());
+        $this->assertEquals($t->getName(), 'test1');
+        $this->assertFalse($t->getCompressAssets());
+        $this->assertNull($t->getImages());
+    }
+}
