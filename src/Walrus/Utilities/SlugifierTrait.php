455a6f862c2473aae9bc077f5c26ba9ca924ddb1
matteo <matteog@gmail.com>
2012-10-21 23:28:43 +0200

    test with locale

--- src/Walrus/Utilities/SlugifierTrait.php
+++ src/Walrus/Utilities/SlugifierTrait.php
@@ -24,6 +24,7 @@ trait SlugifierTrait
      */
     public function slugify($slug, $replacement = '-')
     {
+        setlocale(LC_ALL, 'en_US.UTF8');
         // transliterate
         if (function_exists('iconv')) {
             $slug = iconv('utf-8', 'us-ascii//TRANSLIT', $slug);
@@ -37,7 +38,7 @@ trait SlugifierTrait
         }
 
         // replace non letter or digits with separator
-        $slug = preg_replace('~[^\\pL\d]+~u', '-', $slug);
+        $slug = preg_replace('~[^\\pL\d]+~u', $replacement, $slug);
 
         // remove unwanted characters
         $slug = preg_replace('~[^-\w]+~', '', $slug);
